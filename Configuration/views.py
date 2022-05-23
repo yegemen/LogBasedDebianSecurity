247a8792170e5d31fb49387474bed6b4b62e601f
yegemen <yegemen1616@gmail.com>
2022-05-23 03:56:11 +0300

    block remove

--- Configuration/views.py
+++ Configuration/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 from django.contrib import auth
 from django.contrib import messages
 from .models import mail, trycount, blocklist, settings
+import subprocess
 
 # Create your views here.
 
@@ -31,7 +32,22 @@ def settings(request):
 
 def blockedlist(request):
     if request.method == 'POST':
-        pass
+        ip = request.POST['noblock']
+        service = request.POST['service']
+        blocklist.objects.filter(ip = ip).delete()
+
+        if service == "SSH":
+            delete_rule = subprocess.check_output(f'sudo ufw delete deny proto tcp from {ip} to any port 22', shell=True)
+            delete_log = subprocess.check_output(f"sudo sed '/{ip}/d' /var/log/auth.log -i", shell=True)
+
+        if service == "FTP":
+            pass
+
+        if service == "HTTP":
+            pass
+
+        return redirect('blockedlist')
+
     else:    
         blocks = blocklist.objects.all()
         context = {
